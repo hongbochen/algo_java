d8f773cc157c426c83db840ae3f6808073cf535e
bobo1223 <hongbochen1223@gmail.com>
2021-07-11 22:47:22 +0800

    submit

--- src/site/laoc/leetcode/swap_nodes_in_pairs/SwapNodesInPairs.java
+++ src/site/laoc/leetcode/swap_nodes_in_pairs/SwapNodesInPairs.java
@@ -8,43 +8,47 @@ public class SwapNodesInPairs {
     // 2->1->4->3->6->5
 
     public ListNode swapPairs(ListNode head) {
-        ListNode lastNode = null;
-        ListNode newStart = null;
 
-        int i = 1;
-        ListNode cur = head;
-        ListNode lastLastCoun = null;
+        int size = 0;
 
-        while(cur != null){
-            lastNode = cur;
-            cur = cur.next;
-            i++;
+        ListNode curNode = head;
 
-            if(i % 2 == 0){
-                if(lastNode != null){
-                    System.out.println("cur = " + cur.val + ";pre = " + lastNode.val + ";next=" + cur.next == null ? "-1": cur.next.val);
-                }else{
-                    System.out.println("cur = " + cur.val + ";pre = " + -1 + ";next=" + cur.next == null ? "-1": cur.next.val);
+        while(curNode != null){
+            size++;
+            curNode = curNode.next;
+        }
+
+        if(size == 0 || size == 1) {
+            return head;
+        }
+
+        ListNode nodes[] = new ListNode[size];
+
+        curNode = head;
+        int index = 0;
+        while(curNode != null){
+            nodes[index] = curNode;
+            curNode = curNode.next;
+            index++;
+        }
+
+        // 1->2->3->4->5->6
+        // 2->1->4->3->6->5
+        for(int i = 0;i < nodes.length;i++){
+            if(i % 2 == 1){
+                nodes[i].next = nodes[i-1];
+
+                if(i+1 >= size){
+                    nodes[i-1].next = null;
+                }else if(i + 2 >= size){
+                    nodes[i-1].next = nodes[i+1];
+                }else {
+                    nodes[i - 1].next = nodes[i + 2];
                 }
-
-                // 交换
-                ListNode curNext = cur.next;
-
-                cur.next = lastNode;
-                cur = lastNode;
-                cur.next = curNext;
-
-                if(lastLastCoun != null){
-                    lastLastCoun.next = cur;
-                }else{
-                    newStart = cur;
-                }
-
-                lastLastCoun = lastNode;
             }
         }
 
-        return newStart;
+        return nodes[1];
     }
 
     public void printList(ListNode listNode){
@@ -62,11 +66,11 @@ public class SwapNodesInPairs {
         ListNode node1 = new ListNode(1,null);
         ListNode node2 = new ListNode(2,null);
         ListNode node3 = new ListNode(3,null);
-        ListNode node4 = new ListNode(4,null);
+        //ListNode node4 = new ListNode(4,null);
 
         node1.next = node2;
         node2.next = node3;
-        node3.next = node4;
+        //node3.next = node4;
 
         SwapNodesInPairs swapNodesInPairs  = new SwapNodesInPairs();
         swapNodesInPairs.printList(node1);
